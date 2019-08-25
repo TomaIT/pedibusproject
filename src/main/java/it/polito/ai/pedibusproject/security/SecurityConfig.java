f2f6a630a08b80e88cc482c82c9ca228e03032ec
TomaIT <giovanni.tomasino.it@gmail.com>
2019-08-25 11:17:45 +0200

    add security

--- src/main/java/it/polito/ai/pedibusproject/security/SecurityConfig.java
+++ src/main/java/it/polito/ai/pedibusproject/security/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.DELETE,"/rest/children/**").hasRole("PARENT")
                 .antMatchers(HttpMethod.GET,"/rest/children/**/reservations").hasAnyRole("PARENT","SYS_ADMIN","ADMIN")
                 .antMatchers(HttpMethod.GET,"/rest/children/**/**").hasRole("ESCORT")
+                .antMatchers(HttpMethod.POST,"/rest/children/**/**/**/isTakenWithoutReservation").hasRole("ESCORT")
 
                 //Lines
                 //.antMatchers("/rest/lines/**").hasAnyRole("PARENT","ESCORT","ADMIN","SYS_ADMIN")
