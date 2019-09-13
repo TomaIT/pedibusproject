4b1a75f2e3a44764d22b2f14e322e40f437f06c9
davidesaggese <davide.saggese@studenti.polito.it>
2019-09-13 12:01:18 +0200

    Aggiunto i ReservationState (getIn/getOut/absent) in PresenceChildGET

--- src/main/java/it/polito/ai/pedibusproject/controller/model/get/PresenceChildGET.java
+++ src/main/java/it/polito/ai/pedibusproject/controller/model/get/PresenceChildGET.java
@@ -2,9 +2,8 @@ package it.polito.ai.pedibusproject.controller.model.get;
 
 import it.polito.ai.pedibusproject.database.model.Child;
 import it.polito.ai.pedibusproject.database.model.Reservation;
+import it.polito.ai.pedibusproject.database.model.ReservationState;
 import lombok.Data;
-
-import java.util.Set;
 
 @Data
 public class PresenceChildGET {
@@ -12,18 +11,18 @@ public class PresenceChildGET {
     private String nameChild;
     private boolean isBooked=false;
     private String idReservation=null;
-    private boolean isGetIn=false;
-    private boolean isGetOut=false;
-    private boolean isAbsent=false;
+    private ReservationState getIn;
+    private ReservationState getOut;
+    private ReservationState absent;
 
     public PresenceChildGET(Child child, Reservation reservation){
         this.idChild=child.getId();
         this.nameChild=child.getFirstname()+" "+child.getSurname();
         this.isBooked=true;
         this.idReservation=reservation.getId();
-        this.isGetIn=reservation.getGetIn()!=null;
-        this.isGetOut=reservation.getGetOut()!=null;
-        this.isAbsent=reservation.getAbsent()!=null;
+        this.getIn=reservation.getGetIn();
+        this.getOut=reservation.getGetOut();
+        this.absent=reservation.getAbsent();
     }
 
 }
